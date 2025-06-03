41e944260e9877d1ab2d390dce3b44a67324ff46
joominkyu <empal03@gmail.com>
2025-06-03 23:38:22 +0900

    feat: meta tag, og 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,29 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "empal03 - 단풍지지 - danpungGG",
-  description: "empal03 - 단풍지지 - danpungGG",
+    title: "empal03 - 단풍지지 - 메이플스토리 통계 & 분석 플랫폼",
+    description: "empal03 - 메이플스토리 직업 통계, 길드 분석, 유저 정보 등을 제공하는 단풍지지 플랫폼입니다.",
+    keywords: ["메이플스토리", "직업 통계", "길드 분석", "단풍지지", "MapleStory", "danpungGG"],
+	authors: [{ name: "empal03", url: "https://maple-gg.vercel.app" }],
+    creator: "empal03",
+    publisher: "empal03",
+
+    openGraph: {
+		title: "empal03 - 단풍지지 - 메이플스토리 통계 & 분석 플랫폼",
+		description: "empal03 - 메이플스토리 직업 통계, 길드 분석, 유저 정보 등을 제공하는 단풍지지 플랫폼입니다.",
+		url: "https://maple-gg.vercel.app",
+		siteName: "단풍지지",
+		locale: "ko_KR",
+		type: "website",
+		images: [
+		{
+			url: "/danpungGG.png", // public 폴더에 위치
+			width: 1200,
+			height: 630,
+			alt: "단풍지지 - 메이플 통계 플랫폼",
+		},
+		],
+    },
 };
 
 export default function RootLayout({
