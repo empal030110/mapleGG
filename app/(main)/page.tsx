a1b5f39ce689f7d88b46800c8b58ea7e1b9a7371
joominkyu <empal03@gmail.com>
2025-06-03 22:57:45 +0900

    feat: 메인 헤더 추가

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,7 +1,17 @@
+import Image from "next/image";
+
 export default function Home() {
 	return (
 		<div className="w-full h-full">
-			메인
+			<div className="relative w-full h-[300px] flex items-center justify-center">
+				<Image src="/main/header.png" alt="메인" sizes="(max-width: 768px) 100vw, 940px" fill style={{ objectFit: "cover" }} />
+				<div className="relative w-full text-white flex flex-col gap-[16px] items-center justify-center text-center">
+					<p>메인</p>
+					<div>
+						TODO. 검색 서치바
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
